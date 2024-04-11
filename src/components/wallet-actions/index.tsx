c2f61e39c79dff01daa15691cd0fbccf475b0afd
Vanessa Rodriguez Cristobal <vanessa.cristobal@iohk.io>
2024-04-11 09:04:49 +0100

    fix: refactor calls to reuse same method in index and avoid duplication

--- src/components/wallet-actions/index.tsx
+++ src/components/wallet-actions/index.tsx
@@ -1,11 +1,19 @@
-import { Store, connectorStore } from '../../state/store';
-import { sendCoins, sendSeveralAssets, singleDelegation, singleUndelegation } from '../../features';
-import { useEffect, useState } from 'preact/hooks';
+import { Store, connectorStore } from "../../state/store";
+import {
+  sendCoins,
+  sendSeveralAssets,
+  singleDelegation,
+  singleUndelegation,
+} from "../../features";
+import { useEffect, useState } from "preact/hooks";
 
-import './wallet-actions.css';
+import "./wallet-actions.css";
+import { ObservableWallet } from "@cardano-sdk/wallet";
 
 export const WalletActions = () => {
-  const [storeState, setStoreState] = useState<Store>(connectorStore.initialState);
+  const [storeState, setStoreState] = useState<Store>(
+    connectorStore.initialState
+  );
 
   useEffect(() => {
     const subscription = connectorStore.subscribe(setStoreState);
@@ -20,71 +28,57 @@ export const WalletActions = () => {
     return null;
   }
 
-  const handleSendCoins = async () => {
+  const handleClick = async (
+    action: {
+      ({ connectedWallet }: { connectedWallet: ObservableWallet }): Promise<{
+        hash: string;
+        txId: string;
+      }>;
+      (arg0: { connectedWallet: ObservableWallet }):
+        | PromiseLike<{ hash: any; txId: any }>
+        | { hash: any; txId: any };
+    },
+    title: string
+  ) => {
     if (!storeState.wallet) {
       return null;
     }
 
-    const { hash, txId } = await sendCoins({ connectedWallet: storeState.wallet });
-    connectorStore.log({
-      hash,
-      title: 'Send coins',
-      txId
+    const { hash, txId } = await action({
+      connectedWallet: storeState.wallet,
     });
-  };
-
-  const handleSendSeveralAssets = async () => {
-    if (!storeState.wallet) {
-      return null;
-    }
-
-    const { hash, txId } = await sendSeveralAssets({ connectedWallet: storeState.wallet });
     connectorStore.log({
       hash,
-      title: 'Send several assets',
-      txId
-    });
-  };
-
-  const handleSingleDelegation = async () => {
-    if (!storeState.wallet) {
-      return null;
-    }
-
-    const { hash, txId } = await singleDelegation({ connectedWallet: storeState.wallet });
-    connectorStore.log({
-      hash,
-      title: 'Single delegation',
-      txId
-    });
-  };
-
-  const handleSingleUndelegation = async () => {
-    if (!storeState.wallet) {
-      return null;
-    }
-
-    const { hash, txId } = await singleUndelegation({ connectedWallet: storeState.wallet });
-    connectorStore.log({
-      hash,
-      title: 'Single undelegation',
-      txId
+      title,
+      txId,
     });
   };
 
   return (
     <div class="actions-container">
       <h3>Wallet actions</h3>
-      <button class="wallet-button" onClick={() => handleSendCoins()}>
+      <button
+        class="wallet-button"
+        onClick={() => handleClick(sendCoins, "Send coins")}
+      >
         Send coins
       </button>
-      <button class="wallet-button" onClick={() => handleSendSeveralAssets()}>
+      <button
+        class="wallet-button"
+        onClick={() => handleClick(sendSeveralAssets, "Send several assets")}
+      >
         Send several assets
       </button>
-      <button class="wallet-button" onClick={() => handleSingleDelegation()}>
+      <button
+        class="wallet-button"
+        onClick={() => handleClick(singleDelegation, "Single delegation")}
+      >
         Single delegation
       </button>
-      <button class="wallet-button" onClick={() => handleSingleUndelegation()}>
+      <button
+        class="wallet-button"
+        onClick={() => handleClick(singleUndelegation, "Single undelegation")}
+      >
         Single undelegation
       </button>
     </div>
