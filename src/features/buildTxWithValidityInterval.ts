335d1e0c5cc1f4644ca508642f3c4da35da3579f
Vanessa Rodriguez Cristobal <vanessa.cristobal@iohk.io>
2024-05-08 13:52:09 +0100

    fix: add correct tip

--- src/features/buildTxWithValidityInterval.ts
+++ src/features/buildTxWithValidityInterval.ts
@@ -1,51 +1,44 @@
-import { EMPTY, catchError, take, tap } from "rxjs";
+import { firstValueFrom } from "rxjs";
 import { Cardano } from "@cardano-sdk/core";
-import { logger } from "@cardano-sdk/util-dev";
 import type { ObservableWallet } from "@cardano-sdk/wallet";
 
 import { inspectAndSignTx } from "../utils";
 
-export const buildTxWithValidityInterval = ({
+export const buildTxWithValidityInterval = async ({
   connectedWallet,
   expired,
 }: {
   connectedWallet: ObservableWallet;
   expired: boolean;
-}): Promise<{ hash: string; txId: string }> =>
-  new Promise((resolve, reject) => {
-    connectedWallet.tip$
-      .pipe(
-        take(1),
-        tap(async (tip) => {
-          const expiredValidityInterval: Cardano.ValidityInterval = {
-            invalidHereafter: Cardano.Slot(tip.blockNo),
-          };
-          const noLimitValidityInterval: Cardano.ValidityInterval = {
-            invalidHereafter: undefined,
-          };
+}): Promise<{ hash: string; txId: string } | null> => {
+  if (!connectedWallet) {
+    return null;
+  }
 
-          const builder = connectedWallet.createTxBuilder();
-          const builtTx = builder.addOutput(
-            await builder.buildOutput().handle("rhys").coin(10_000_000n).build()
-          );
-          const expiredValidityIntervalTx = builtTx
-            .setValidityInterval(
-              expired ? expiredValidityInterval : noLimitValidityInterval
-            )
-            .build();
+  const tip = await firstValueFrom(connectedWallet.tip$);
 
-          const { hash, txId } = await inspectAndSignTx({
-            builtTx: expiredValidityIntervalTx,
-            connectedWallet,
-          });
+  const expiredValidityInterval: Cardano.ValidityInterval = {
+    invalidHereafter: Cardano.Slot(tip.blockNo),
+  };
 
-          resolve({ hash, txId });
-        }),
-        catchError((error) => {
-          logger.error("Failed to build tx", error);
-          reject(new Error("Failed to build transaction"));
-          return EMPTY;
-        })
-      )
-      .subscribe();
+  const noLimitValidityInterval: Cardano.ValidityInterval = {
+    invalidHereafter: undefined,
+  };
+
+  const builder = connectedWallet.createTxBuilder();
+  const builtTx = builder.addOutput(
+    await builder.buildOutput().handle("rhys").coin(10_000_000n).build()
+  );
+  const expiredValidityIntervalTx = builtTx
+    .setValidityInterval(
+      expired ? expiredValidityInterval : noLimitValidityInterval
+    )
+    .build();
+
+  const { hash, txId } = await inspectAndSignTx({
+    builtTx: expiredValidityIntervalTx,
+    connectedWallet,
   });
+
+  return { hash, txId };
+};
